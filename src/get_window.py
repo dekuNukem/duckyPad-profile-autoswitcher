9c083305f921ac52888cef52b7eb5ac2766f522f
Andrei Moldovan <andrei.moldovan@fourth.com>
2021-05-10 21:40:22 +0100

    first go at making it work on MacOS

--- src/get_window.py
+++ src/get_window.py
@@ -1,12 +1,56 @@
-import wmi
 import time
-import win32process
-import pygetwindow as gw
+import platform
 
-c = wmi.WMI()
+p = platform.system()
+if p == 'Windows':
+    import wmi
+    import win32process
+    import pygetwindow as gw
+elif p == 'Darwin':
+    from AppKit import NSWorkspace
+    import Quartz
 
-def get_app_name(hwnd):
+
+def get_active_window():
+    if p == 'Windows':
+        return win_get_active_window()
+    elif p == 'Darwin':
+        return darwin_get_active_window()
+    raise 'Platform %s not supported' % p
+
+
+def get_list_of_all_windows():
+    if p == 'Windows':
+        return win_get_list_of_all_windows()
+    elif p == 'Darwin':
+        return darwin_get_list_of_all_windows()
+    raise 'Platform %s not supported' % p
+
+def darwin_get_active_window():
+    windows = Quartz.CGWindowListCopyWindowInfo(
+        Quartz.kCGWindowListExcludeDesktopElements | Quartz.kCGWindowListOptionOnScreenOnly, Quartz.kCGNullWindowID)
+    for window in windows:
+        if window[Quartz.kCGWindowLayer] == 0:
+            return window[Quartz.kCGWindowOwnerName], window.get(Quartz.kCGWindowName, 'unknown')
+
+    return None, None
+
+
+def darwin_get_list_of_all_windows():
+    apps = []
+    windows = Quartz.CGWindowListCopyWindowInfo(
+        Quartz.kCGWindowListExcludeDesktopElements | Quartz.kCGWindowListOptionOnScreenOnly, Quartz.kCGNullWindowID)
+
+    for window in windows:
+        apps.append((window[Quartz.kCGWindowOwnerName],
+                    window.get(Quartz.kCGWindowName, 'unknown')))
+    apps = sorted(apps, key=lambda x: x[0])
+    return apps
+
+
+def win_get_app_name(hwnd):
     """Get application name given hwnd."""
+    c = wmi.WMI()
     try:
         _, pid = win32process.GetWindowThreadProcessId(hwnd)
         for p in c.query('SELECT Name FROM Win32_Process WHERE ProcessId = %s' % str(pid)):
@@ -17,17 +61,17 @@ def get_app_name(hwnd):
     else:
         return exe
 
-# returns a list of (app_name, window_title) tuples
-def get_list_of_all_windows():
-	ret = set()
-	for item in gw.getAllWindows():
-		ret.add((get_app_name(item._hWnd), item.title))
-	ret = sorted(list(ret), key=lambda x: x[0])
-	return ret
 
-# returns a (hWnd, app_name, window_title) tuple
-def get_active_window():
-	active_window = gw.getActiveWindow()
-	if active_window is None:
-		return None, None, None
-	return (active_window._hWnd, get_app_name(active_window._hWnd), active_window.title)
+def win_get_list_of_all_windows():
+    ret = set()
+    for item in gw.getAllWindows():
+        ret.add((win_get_app_name(item._hWnd), item.title))
+    ret = sorted(list(ret), key=lambda x: x[0])
+    return ret
+
+
+def win_get_active_window():
+    active_window = gw.getActiveWindow()
+    if active_window is None:
+        return None, None
+    return (win_get_app_name(active_window._hWnd), active_window.title)
