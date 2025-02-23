85a3a1e91d7ddf034ff1d2f61d538b0f8fdb8c3f
dekuNukem <dekunukem@gmail.com>
2025-02-23 19:25:31 +0000

    testing dump

--- hid_example/dump.py
+++ hid_example/dump.py
@@ -15,13 +15,19 @@ DUCKYPAD_TO_PC_HID_BUF_SIZE = 64
 
 h = hid.device()
 
+def millis():
+	return time.time_ns() // 1000000;
+
+pgm_start = millis()
+
 duckypad_pid = 0xd11c
 duckypad_pro_pid = 0xd11d
+valid_pid_list = [duckypad_pro_pid, duckypad_pid]
 
-def get_path_by_pid(my_pid):
+def get_duckypad_path_uncached():
     path_dict = {}
     for device_dict in hid.enumerate():
-        if device_dict['vendor_id'] == 0x0483 and device_dict['product_id'] == my_pid:
+        if device_dict['vendor_id'] == 0x0483 and device_dict['product_id'] in valid_pid_list:
             path_dict[device_dict['usage']] = device_dict['path']
     if len(path_dict) == 0:
         return None
@@ -29,14 +35,14 @@ def get_path_by_pid(my_pid):
         return path_dict[58]
     return list(path_dict.values())[0]
 
-def get_duckypad_path():
-    dpp_path = get_path_by_pid(duckypad_pro_pid)
-    if dpp_path is not None:
-        return dpp_path
-    dp_path = get_path_by_pid(duckypad_pid)
-    if dp_path is not None:
-        return dp_path
-    return None
+last_dp_path = None
+def get_duckypad_path(start_fresh=False):
+    global last_dp_path
+    if start_fresh:
+        last_dp_path = None
+    if last_dp_path is None:
+        last_dp_path = get_duckypad_path_uncached()
+    return last_dp_path
 
 # wait up to 0.5 seconds for response
 def hid_read():
@@ -48,28 +54,23 @@ def hid_read():
 		time.sleep(0.01)
 	return []
 
-def duckypad_hid_write(hid_buf_64b):
-	if len(hid_buf_64b) != PC_TO_DUCKYPAD_HID_BUF_SIZE:
-		raise ValueError('PC-to-duckyPad buffer wrong size, should be exactly 64 Bytes')
-	duckypad_path = get_duckypad_path()
-	if duckypad_path is None:
-		raise OSError('duckyPad Not Found!')
-	h.open_path(duckypad_path)
-	h.set_nonblocking(1)
-	h.write(hid_buf_64b)
-	result = hid_read()
-	h.close()
-	return result
-
 pc_to_duckypad_buf = [0] * PC_TO_DUCKYPAD_HID_BUF_SIZE
 pc_to_duckypad_buf[0] = 5	# HID Usage ID, always 5
 pc_to_duckypad_buf[1] = 0	# Sequence Number
-pc_to_duckypad_buf[2] = 32	# Command type
-pc_to_duckypad_buf[3] = 2	# Action: start/abort
-
-print("\n\nSending to duckyPad:\n", pc_to_duckypad_buf)
-duckypad_to_pc_buf = duckypad_hid_write(pc_to_duckypad_buf)
-print("\nduckyPad response:\n", duckypad_to_pc_buf)
+pc_to_duckypad_buf[2] = 0	# Command type
 
 
+duckypad_path = get_duckypad_path()
+if duckypad_path is None:
+	raise OSError('duckyPad Not Found!')
+h.open_path(duckypad_path)
+h.set_nonblocking(1)
 
+for x in range(10):
+	# print("\n\nSending to duckyPad:\n", pc_to_duckypad_buf)
+	h.write(pc_to_duckypad_buf)
+	duckypad_to_pc_buf = hid_read()
+	# print("\nduckyPad response:\n", duckypad_to_pc_buf)
+	print(x, millis() - pgm_start)
+
+h.close()
