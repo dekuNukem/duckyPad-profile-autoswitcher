07c4f74396004009497ecc9dad908bed637f0028
dekuNukem <dekunukem@gmail.com>
2025-02-23 18:07:04 +0000

    testing out new dumping protocol

---
+++ hid_example/dump.py
@@ -0,0 +1,75 @@
+"""
+duckyPad HID example: HID read AND write
+
+For more details, see:
+
+https://github.com/dekuNukem/duckyPad-profile-autoswitcher/blob/master/HID_details.md
+
+"""
+
+import hid
+import time
+
+PC_TO_DUCKYPAD_HID_BUF_SIZE = 64
+DUCKYPAD_TO_PC_HID_BUF_SIZE = 64
+
+h = hid.device()
+
+duckypad_pid = 0xd11c
+duckypad_pro_pid = 0xd11d
+
+def get_path_by_pid(my_pid):
+    path_dict = {}
+    for device_dict in hid.enumerate():
+        if device_dict['vendor_id'] == 0x0483 and device_dict['product_id'] == my_pid:
+            path_dict[device_dict['usage']] = device_dict['path']
+    if len(path_dict) == 0:
+        return None
+    if 58 in path_dict:
+        return path_dict[58]
+    return list(path_dict.values())[0]
+
+def get_duckypad_path():
+    dpp_path = get_path_by_pid(duckypad_pro_pid)
+    if dpp_path is not None:
+        return dpp_path
+    dp_path = get_path_by_pid(duckypad_pid)
+    if dp_path is not None:
+        return dp_path
+    return None
+
+# wait up to 0.5 seconds for response
+def hid_read():
+	read_start = time.time()
+	while time.time() - read_start <= 1:
+		result = h.read(DUCKYPAD_TO_PC_HID_BUF_SIZE)
+		if len(result) > 0:
+			return result
+		time.sleep(0.01)
+	return []
+
+def duckypad_hid_write(hid_buf_64b):
+	if len(hid_buf_64b) != PC_TO_DUCKYPAD_HID_BUF_SIZE:
+		raise ValueError('PC-to-duckyPad buffer wrong size, should be exactly 64 Bytes')
+	duckypad_path = get_duckypad_path()
+	if duckypad_path is None:
+		raise OSError('duckyPad Not Found!')
+	h.open_path(duckypad_path)
+	h.set_nonblocking(1)
+	h.write(hid_buf_64b)
+	result = hid_read()
+	h.close()
+	return result
+
+pc_to_duckypad_buf = [0] * PC_TO_DUCKYPAD_HID_BUF_SIZE
+pc_to_duckypad_buf[0] = 5	# HID Usage ID, always 5
+pc_to_duckypad_buf[1] = 0	# Sequence Number
+pc_to_duckypad_buf[2] = 32	# Command type
+pc_to_duckypad_buf[3] = 2	# Action: start/abort
+
+print("\n\nSending to duckyPad:\n", pc_to_duckypad_buf)
+duckypad_to_pc_buf = duckypad_hid_write(pc_to_duckypad_buf)
+print("\nduckyPad response:\n", duckypad_to_pc_buf)
+
+
+
